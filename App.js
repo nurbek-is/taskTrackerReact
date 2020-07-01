ab4c0efebde560c1fd84448a8f683aa5326692de
nurbek82 <nurbek82@gmail.com>
2020-07-01 15:36:47 -0700

    set up basic react app
    the complete app should look like a kanban, a task tracker for productivity

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+
+
+
+render () {
+  return (
+    <div className="container">
+      <div className="box">1</div>
+      <div className="box">2</div>
+      <div className="box">3</div>
+      <div className="box">4</div>
+    </div>
+  );
+}
+}
+
+export default App;

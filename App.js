b4f9d025a23b3c78255af35419f62e502bb4bdb4
nurbek82 <nurbek82@gmail.com>
2020-07-06 15:41:58 -0700

    UI is done
    Static UI is done

--- App.js
+++ App.js
@@ -5,23 +5,47 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
     }
   }
-
-
-
 
 render () {
   return (
     <div className="container">
-      <div className="box">1</div>
-      <div className="box">2</div>
-      <div className="box">3</div>
-      <div className="box">4</div>
+      <div className="box backlog">
+      <div className='header card'>Backlog</div>
+      <div className='card'>Implement Chat</div>
+      <div className='card'>Implement Chat</div>
+      <div className='card'>Implement REST backend</div>
+      <div className='add-card'> + Add Card</div>
     </div>
-  );
-}
+
+      <div className="box todo">
+      <div className='header card'>To Do</div>
+      <div className='card'>Implement Chat</div>
+      <div className='card'>Fix JS code</div>
+      <div className='card'>Clean HTML</div>
+      <div className='add-card'> + Add Card</div>
+      </div>
+      
+      <div className="box progress">
+      <div className='header card'>Progress</div>
+      <div className='card'>Implement Chat</div>
+      <div className='card'>Fix JS code</div>
+      <div className='card'>Clean HTML</div>
+      <div className='add-card'> + Add Card</div>
+      </div>
+
+      <div className="box done">
+      <div className='header card'>Done</div>
+      <div className='card'>Implement Chat</div>
+      <div className='card'>Fix JS code</div>
+      <div className='card'>Clean HTML</div>
+      <div className='add-card'> + Add Card</div>
+      </div>
+
+    </div>
+    );
+  }
 }
 
 export default App;

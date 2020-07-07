7f8678a9463a0940d41403db68e0a8447adc3513
nurbek82 <nurbek82@gmail.com>
2020-07-07 12:36:52 -0700

    added moveRight function
    that moves the card from Backlog column to ToDo

--- App.js
+++ App.js
@@ -20,33 +20,68 @@ addCard = (name) => {
   box.push(card);
   this.setState({[name]:box})
 }
+
+moveRight = (name,ind) => {
+  //get box from state
+  const box = this.state[name];
+  const todo = this.state.todo;
+  //add card into next box
+  todo.push(box[ind]);
+  //remove card from the current box
+  box.splice(ind,1)
+  //Setting State
+  this.setState({todo,[name]:box})
+}
 render () {
   return (
     <div className="container">
       <div className="box backlog">
       <div className='header card'>Backlog</div>
-      {this.state.backlog.map((card,ind) => <div key={String(ind)} className='card'>{card}</div>
-       )}
+      {this.state.backlog.map((card,ind) => { 
+        return  (<div key={String(ind)} className='card'>
+        {card}
+      <span onClick={() => this.moveRight('backlog', ind)} className="move-right">right</span>
+        </div>
+      )
+      })}
       <div onClick ={()=> this.addCard('backlog')} className='add-card'> + Add Card</div>
     </div>
       <div className="box todo">
       <div className='header card'>To Do</div>
-      {this.state.todo.map((card,ind) => <div key={String(ind)} className='card'>{card}</div>
-       )}
+      {this.state.todo.map((card,ind) => { 
+        return  (<div key={String(ind)} className='card'>
+        <span className="move-left">left</span>
+        {card}
+        <span className="move-right">right</span>
+        </div>
+      )
+      })}
       <div onClick ={()=>this.addCard('todo')} className='add-card'> + Add Card</div>
       </div>
       
       <div className="box progress">
       <div className='header card'>Progress</div>
-      {this.state.progress.map((card,ind) => <div key={String(ind)} className='card'>{card}</div>
-       )}
+      {this.state.progress.map((card,ind) => { 
+        return  (<div key={String(ind)} className='card'>
+        <span className="move-left">left</span>
+        {card}
+        <span className="move-right">right</span>
+        </div>
+      )
+      })}
       <div onClick ={()=> this.addCard("progress")} className='add-card'> + Add Card</div>
       </div>
 
       <div className="box done">
       <div className='header card'>Done</div>
-      {this.state.done.map((card,ind) => <div key={String(ind)} className='card'>{card}</div>
-       )}
+      {this.state.done.map((card,ind) => { 
+        return  (<div key={String(ind)} className='card'>
+        <span className="move-left">left</span>
+        {card}
+        <span className="move-right">right</span>
+        </div>
+      )
+      })}
        <div onClick ={() => this.addCard('done')} className='add-card'> + Add Card</div>
       </div>
     </div>
